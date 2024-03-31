88213564bc2f6181f402a4f8ef20364895bc869a
Documenter.jl <documenter@juliadocs.github.io>
2024-03-31 16:11:27 +0000

    build based on 5d8b649

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PBRT.jl","page":"PBRT.jl","title":"PBRT.jl","text":"","category":"section"},{"location":"","page":"PBRT.jl","title":"PBRT.jl","text":"Documentation for PBRT.jl","category":"page"},{"location":"","page":"PBRT.jl","title":"PBRT.jl","text":"greet_your_package_name()","category":"page"},{"location":"#PBRT.greet_your_package_name-Tuple{}","page":"PBRT.jl","title":"PBRT.greet_your_package_name","text":"greet_your_package_name()\n\nReturn a greeting message.\n\n\n\n\n\n","category":"method"}]
+}
