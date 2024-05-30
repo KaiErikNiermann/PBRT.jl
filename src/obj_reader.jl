502a10eef86c23a24983e7b4e9169fff32fc20e7
KaiErikNiermann <kai.niermann@gmail.com>
2024-05-30 08:07:25 +0000

    feat: testing notebook

--- src/obj_reader.jl
+++ src/obj_reader.jl
@@ -6,9 +6,9 @@ mutable struct obj <: hittable
 end
 
 mutable struct face
-    vertices::Vector{Int64}
-    normals::Vector{Int64}
-    textures::Vector{Int64}
+    vertices::Vector{Vector{Float64}}
+    normals::Vector{Vector{Float64}}
+    textures::Vector{Vector{Float64}}
 end
 
 mutable struct obj_scene
@@ -73,23 +73,23 @@ function reader(file_name::String)::obj_scene
             # faces 
             startswith(line, "f ") => begin
                 f = split(line[3:end], " ")
-                vertices = Vector{Float64}()
-                normals = Vector{Float64}()
-                textures = Vector{Float64}()
+                vertices = Vector{Vector{Float64}}()
+                normals = Vector{Vector{Float64}}()
+                textures = Vector{Vector{Float64}}()
                 for i in f
                     @match true begin
                         # vertex, texture, normal
                         occursin(r"\d+\/\d+\/\d+", i) => begin
                             face = split(i, "/")
-                            push!(vertices, parse(Int64, v_array[face[1]]))
-                            push!(textures, parse(Int64, vt_array[face[2]]))
-                            push!(normals, parse(Int64, vn_array[face[3]]))
+                            push!(vertices, v_array[parse(Int64, face[1])])
+                            push!(textures, vt_array[parse(Int64, face[2])])
+                            push!(normals, vn_array[parse(Int64, face[3])])
                         end
                         # vertex, normal
                         occursin(r"\d+\/\/\d+", i) => begin
                             face = split(i, "//")
-                            push!(vertices, parse(Int64, v_array[face[1]]))
-                            push!(normals, parse(Int64, vn_array[face[2]]))
+                            push!(vertices, v_array[parse(Int64, face[1])])
+                            push!(normals, vn_array[parse(Int64, face[2])])
                         end
                     end
                 end
@@ -136,8 +136,35 @@ end
 """
 Function to check if a point is inside the circumcircle of a triangle
 """
-function in_circumcircle(t::triangle, v::Vector{Float64})
+function in_circumcircle(t::triangle, v::Vector{Float64}, vn::Vector{Float64})::Bool
+    k = [0, 0, 1]
+    rot_angle = acos(dot(vn, k) / (norm(vn) * norm(k)))
+    rot_matrix = [cos(rot_angle) -sin(rot_angle) 0; sin(rot_angle) cos(rot_angle) 0; 0 0 1]
     
+    P1 = t.A 
+    P2 = t.B
+    P3 = t.C
+
+    P1_rot = rot_matrix * P1
+    P2_rot = rot_matrix * P2
+    P3_rot = rot_matrix * P3 
+    v_rot = rot_matrix * v
+
+    P1_2D = [P1_rot[1], P1_rot[2]]
+    P2_2D = [P2_rot[1], P2_rot[2]]
+    P3_2D = [P3_rot[1], P3_rot[2]]
+    v_2D = [v_rot[1], v_rot[2]]
+
+    p1_x = P1_2D[1] - v_2D[1]
+    p1_y = P1_2D[2] - v_2D[2]
+    p2_x = P2_2D[1] - v_2D[1]
+    p2_y = P2_2D[2] - v_2D[2]
+    p3_x = P3_2D[1] - v_2D[1]
+    p3_y = P3_2D[2] - v_2D[2]
+
+    return (p1_x^2 + p1_y^2) * (p2_x * p3_y - p2_y * p3_x) - 
+           (p2_x^2 + p2_y^2) * (p1_x * p3_y - p1_y * p3_x) + 
+           (p3_x^2 + p3_y^2) * (p1_x * p2_y - p1_y * p2_x) > 0
 end
 
 """
@@ -158,8 +185,8 @@ function triangulation(faces::Vector{face})::Vector{triangle}
     for (v, vn) in zip(faces.vertices, faces.normals)
         bad_triangles = Vector{triangle}()
         for t in triangles
-            if in_circumcircle(t, v)
-                push!(bad_triangles, t, vn)
+            if in_circumcircle(t, v, vn)
+                push!(bad_triangles, t)
             end
         end
         polygon = Vector{Vector{Int64}}()   
@@ -191,7 +218,11 @@ scene_summary(sc)
 
 faces = sc.f_array
 
-triangle_1 = faces[1]
+face_1 = faces[10]
+triangle_1 = triangle(face_1.vertices[1], face_1.vertices[2], face_1.vertices[3])
+v1 = face_1.vertices[4]
 
-println(triangle_1.vertices)
+println(triangle_1)
+println(v1)
 
+print(in_circumcircle(triangle_1, v1, face_1.normals[1]))
--- src/triangle.jl
+++ src/triangle.jl
@@ -1,10 +1,14 @@
 struct triangle <: hittable
-    x::Float64
-    y::Float64
-    z::Float64
+    A::Vector{Float64}
+    B::Vector{Float64}
+    C::Vector{Float64}
     bbox::aabb
-    edges::Vector{Vector{Int64}}
-    triangle(x, y, z) = new(x, y, z, aabb(), Vector{Vector{Int64}}())
+    edges::Vector{Vector{Vector{Float64}}}
+    triangle(A::Vector{Float64}, B::Vector{Float64}, C::Vector{Float64}) = begin 
+        bbox = aabb()
+        edges = [[A, B], [B, C], [C, A]]
+        new(A, B, C, bbox, edges)
+    end
 end 
 
 function hit!(t::triangle, r::ray, ray_t::interval, rec::hit_record)::Bool
